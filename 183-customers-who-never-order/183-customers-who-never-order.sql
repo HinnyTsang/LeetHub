60854b6e9c9ccc6b9b7c7b74f08927c7b82eff44
Hinny Tsang <46905161+HinnyTsang@users.noreply.github.com>
2022-05-09 15:48:38 +0800

    Time: 374 ms (97.52%), Space: 0B (100.00%) - LeetHub

---
+++ 183-customers-who-never-order/183-customers-who-never-order.sql
@@ -0,0 +1,21 @@
+# Write your MySQL query statement below
+
+# SELECT name
+# FROM (
+#     Customers C
+#     RIGHT JOIN
+#     Orders O
+#     ON 
+#     C.id = O.customerId 
+# ) ;
+    
+
+SELECT name AS "Customers"
+FROM
+    Customers C  
+LEFT JOIN
+    Orders O  
+ON 
+    C.id = O.customerId 
+WHERE 
+    O.customerId IS NULL;
