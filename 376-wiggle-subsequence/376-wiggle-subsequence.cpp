ea5fc087b17c73782c4fdacbfa13256d9dffe1c2
Hinny Tsang <46905161+HinnyTsang@users.noreply.github.com>
2022-05-20 17:58:49 +0800

    Time: 0 ms (100.00%), Space: 7.2 MB (41.68%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -4,24 +4,15 @@ public:
         
         int n = nums.size();
         
-        vector<int> up(n, 1);
-        vector<int> dw(n, 1);
+        int up = 1, dw = 1;
         
         for (int i = 1; i < n; ++i) {
-            if (nums[i] > nums[i - 1]) {
-                up[i] = dw[i-1] + 1;
-                dw[i] = dw[i-1];
-            }
-            else if (nums[i] < nums[i-1]) {
-                up[i] = up[i-1];
-                dw[i] = up[i-1] + 1;
-            }
-            else {
-                up[i] = up[i-1];
-                dw[i] = dw[i-1];
-            }
+            if (nums[i] > nums[i - 1])
+                up = dw + 1;
+            else if (nums[i] < nums[i-1])
+                dw = up + 1;
         }
         
-        return max(up.back(), dw.back());
+        return max(dw, up);
     }
 };
