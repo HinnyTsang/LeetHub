f71f8411c8f843b084f4869852176974c875e6b8
Hinny Tsang <46905161+HinnyTsang@users.noreply.github.com>
2022-05-22 14:55:36 +0800

    Time: 30 ms (51.20%), Space: 7.3 MB (74.61%) - LeetHub

---
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int change(int n, vector<int>& coins) {
+        
+        sort(coins.begin(), coins.end());
+        
+        vector<int> n_coins(n+1, 0);
+        
+        // no coin.
+        n_coins[0] = 1;
+        
+        for (int j = 0; j < coins.size(); ++j) {
+            for (int i = 1; i <= n ; ++i) {
+                if (coins[j] <= i)
+                    n_coins[i] += n_coins[i - coins[j]];
+            }
+        }
+        
+        return n_coins.back();
+    }
+};
