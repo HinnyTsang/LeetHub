df76d0ff86f260e19ac14e1616b1b70783e5e05f
Hinny Tsang <46905161+HinnyTsang@users.noreply.github.com>
2022-05-23 16:57:21 +0800

    Time: 239 ms (51.41%), Space: 9 MB (61.63%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -6,12 +6,11 @@ public:
         
         iota(dp.begin(), dp.end(), 0);
         
-        for (int val = 2; val <= n; ++val) {
-            for (int sq = 2; val >= sq*sq; ++sq) {
-                dp[val] = min(dp[val], dp[val - sq*sq] + 1);
+        for (int i = 0; i <= n; ++i) {
+            for (int sq = 1; sq * sq <= i; ++sq) {
+                dp[i] = min(dp[i], dp[i - sq*sq] + 1);    
             }
         }
-        
         
         return dp.back();
     }
