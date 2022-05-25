2f58ade11c76e29295202c3ba546887184c4d36a
Hinny Tsang <46905161+HinnyTsang@users.noreply.github.com>
2022-05-25 10:56:47 +0800

    Time: 688 ms (15.63%), Space: 77.6 MB (44.39%) - LeetHub

---
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        
+        sort(envelopes.begin(), envelopes.end(), [](vector<int> &a,vector<int> &b){
+            return (a[0] < b[0] || (a[0] == b[0] && (a[1] > b[1])));
+        });
+        
+        int n = envelopes.size();
+        vector<int> dp;
+        
+        for (int i = 0; i < n; ++i) {
+            auto it = lower_bound(dp.begin(), dp.end(), envelopes[i][1]);
+            
+            if (it == dp.end()) {
+                dp.push_back(envelopes[i][1]);
+            }
+            else {
+                *it = envelopes[i][1];
+            }
+        }
+        
+        return dp.size();
+    }
+};
