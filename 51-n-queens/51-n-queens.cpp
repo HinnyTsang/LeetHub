90ca48d8c58ee626f1655e8db5265fc64e91eb99
Hinny Tsang <46905161+HinnyTsang@users.noreply.github.com>
2022-06-04 18:53:26 +0800

    Time: 6 ms (79.52%), Space: 7 MB (99.59%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,12 +1,13 @@
 class Solution {
 private:
     int N;
-    void backtracking(string& row, string& col, string& off, string& diag,
-                      set<vector<string>>& result, vector<string>& temp,
-                      int r) 
+    void backtracking(
+        string& col, string& off, string& diag,
+        vector<vector<string>>& result, vector<string>& temp,
+        int r) 
     {
         if (r == N) {
-            result.insert(temp);
+            result.push_back(temp);
             return;
         }
         
@@ -14,9 +15,9 @@ private:
             if (col[c] == 'Q' || diag[N-1-r+c] == 'Q' || off[r+c] == 'Q') {
                 continue;
             }
-            col[c] = row[r] = diag[N-1-r+c] = off[r+c] = temp[r][c] = 'Q';
-            backtracking(row, col, off, diag, result, temp, r + 1);
-            col[c] = row[r] = diag[N-1-r+c] = off[r+c] = temp[r][c] = '.';
+            col[c] = diag[N-1-r+c] = off[r+c] = temp[r][c] = 'Q';
+            backtracking(col, off, diag, result, temp, r + 1);
+            col[c] = diag[N-1-r+c] = off[r+c] = temp[r][c] = '.';
         }
         
     }
@@ -24,12 +25,12 @@ public:
     vector<vector<string>> solveNQueens(int n) {
         
         N = n;
-        set<vector<string>> result;
+        vector<vector<string>> result;
         vector<string> temp(n, string(n, '.'));
-        string diag(2*n - 1, '.'), off(2*n - 1, '.'), row(n, '.'), col(n, '.');
+        string diag(2*n - 1, '.'), off(2*n - 1, '.'), col(n, '.');
         
-        backtracking(row, col, off, diag, result, temp, 0);
+        backtracking(col, off, diag, result, temp, 0);
 
-        return vector<vector<string>> (result.begin(), result.end());
+        return result;
     }
 };
