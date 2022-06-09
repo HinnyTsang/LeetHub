c05cf24048373f66f2d5bc7d8069d20cbb6a7d45
Hinny Tsang <46905161+HinnyTsang@users.noreply.github.com>
2022-06-09 13:18:58 +0800

    Time: 174 ms (92.03%), Space: 89 MB (40.55%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.cpp
+++ 739-daily-temperatures/739-daily-temperatures.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        
-        static auto cmp = [](pair<int, int>& a, pair<int, int>& b) {
-            return a.first > b.first;
-        };
-        
-        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp) > prev_days(cmp);
+
+        stack<int> prev_days;
         vector<int> result(temperatures.size(), 0);
         
         for (int i = 0; i < temperatures.size(); ++i) {
-            while (prev_days.size() > 0 && prev_days.top().first < temperatures[i]) {
-                auto [temp, day] = prev_days.top(); prev_days.pop();
+            while (prev_days.size() > 0 && temperatures[prev_days.top()] < temperatures[i]) {
+                auto day = prev_days.top(); prev_days.pop();
                 result[day] = i - day;
             }
-            prev_days.emplace(temperatures[i], i);
+            prev_days.push(i);
         }
         return result;
     }
