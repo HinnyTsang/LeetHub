c5e86b0728f667d1de7840f8b5489aa7045fe1e3
Hinny Tsang <46905161+HinnyTsang@users.noreply.github.com>
2022-06-14 20:45:48 +0800

    Time: 541 ms (37.22%), Space: 124.3 MB (46.10%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,32 +1,38 @@
 class Solution {
-
-private:
-    void dfs(vector<vector<int>>& tree, int i, vector<int>& informTime, int& time) {
-        
-        if (tree[i].size() == 0) {
-            time = max(time, informTime[i]);
-        }
-        
-        for (int& j: tree[i]) {
-            informTime[j] += informTime[i];
-            dfs(tree, j, informTime, time);
-        }
-    }
-    
 public:
-    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-        vector<vector<int>> tree(n);
+    int numOfMinutes(int n, int headID, vector<int>& managers, vector<int>& informTime) {
+        
+        vector<vector<int>> children(n, vector<int>());
+        queue<int> manager;
+        int max_time = 0;
         
         for (int i = 0; i < n; ++i) {
-            if (manager[i] != -1) {
-                tree[manager[i]].push_back(i);
+            if (i == headID) continue;
+            
+            if (managers[i] == headID) {
+                informTime[i] += informTime[headID];
+                manager.push(i);
+                max_time = max(max_time, informTime[i]);
+            }
+            else {
+                children[managers[i]].push_back(i);
+            }   
+        }
+        
+        while (manager.size() > 0) {
+            int l = manager.size();
+            
+            while (l-- > 0) {
+                int employee = manager.front(); manager.pop();
+                
+                for (auto& i: children[employee]) {
+                    informTime[i] += informTime[employee];
+                    manager.push(i);
+                    max_time = max(max_time, informTime[i]);
+                }
             }
         }
         
-        int time = 0;
-        
-        dfs(tree, headID, informTime, time);
-        
-        return time;
+        return max_time;
     }
 };
