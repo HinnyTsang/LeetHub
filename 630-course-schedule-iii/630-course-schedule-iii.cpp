2da067319c9f857448fede4307d713f5667076ce
Hinny Tsang <46905161+HinnyTsang@users.noreply.github.com>
2022-06-23 13:48:05 +0800

    Time: 281 ms (78.69%), Space: 56.2 MB (85.57%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -2,15 +2,9 @@ class Solution {
 public:
     int scheduleCourse(vector<vector<int>>& courses) {
         
-        static auto compare = [](auto& a, auto& b) {
-            return a.first < b.first;        
-        };
+        priority_queue<int> taked_course;
         
-        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(compare)> taked_course(compare);
-        
-        sort(courses.begin(), courses.end(), [](auto& a, auto&b) {
-            return a[1] < b[1];    
-        });
+        sort(courses.begin(), courses.end(), [](auto& a, auto&b) { return a[1] < b[1];});
         
         int totalTime = 0;
         
@@ -20,13 +14,13 @@ public:
                 continue;
             
             if (totalTime + course[0] <= course[1]) {
-                taked_course.emplace(course[0], course[1]);
+                taked_course.push(course[0]);
                 totalTime += course[0];
             }
-            else if (taked_course.top().first > course[0]) {
-                totalTime += course[0] - taked_course.top().first;
+            else if (taked_course.top() > course[0]) {
+                totalTime += course[0] - taked_course.top();
                 taked_course.pop();
-                taked_course.emplace(course[0], course[1]);
+                taked_course.push(course[0]);
             }
         }
         
