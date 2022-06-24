f14b0e371a208949bfc0f3c59e6db2e740277f9d
Hinny Tsang <46905161+HinnyTsang@users.noreply.github.com>
2022-06-24 19:49:33 +0800

    Time: 9 ms (66.64%), Space: 14.7 MB (81.20%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,8 +11,7 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if (!list1 && !list2) return nullptr;
-        
+
         if (!list1 || !list2) return list1? list1: list2;
         
         if (list1->val < list2->val) {
