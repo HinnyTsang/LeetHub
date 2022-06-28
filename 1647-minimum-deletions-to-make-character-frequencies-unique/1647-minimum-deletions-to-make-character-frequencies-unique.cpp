14995a4f6a0a0d97d8af28f95177d1be53aa0d65
Hinny Tsang <46905161+HinnyTsang@users.noreply.github.com>
2022-06-28 12:36:06 +0800

    Time: 96 ms (48.28%), Space: 17.4 MB (37.46%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -5,14 +5,10 @@ public:
         unordered_set<int> freq;
         int result = 0;
         
-        for (auto& c: s) {
+        for (auto& c: s)
             ++char_count[c - 'a'];
-        }
-        
-        
+
         for (int i = 0; i < 26; ++i) {
-            if (char_count[i] == 0)
-                continue;
             while (char_count[i] > 0 && freq.count(char_count[i])) {
                 --char_count[i];
                 ++result;
