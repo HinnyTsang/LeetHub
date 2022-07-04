e93dab57e6ec2f73b55752b385a8530a4f7530c5
Hinny Tsang <46905161+HinnyTsang@users.noreply.github.com>
2022-07-04 15:32:51 +0800

    Time: 0 ms (100.00%), Space: 5.9 MB (90.19%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        if (m > n) swap (m, n);
+        if (m > n) swap(m, n);
         
-        vector<int> dp(m, 1);
+        int* dp = new int [m];
         
-        for (int r = 1; r < n; ++r) {
-            for (int c = 1; c < m; ++c) {
-                dp[c] += dp[c-1];
+        for (int i = 0; i < m; ++i)
+            dp[i] = 1;
+        
+        for (int i = 1; i < n; ++i) {
+            for (int j = 1; j < m; ++j) {
+                dp[j] += dp[j-1];
             }
         }
         
-        return dp[m-1];
+        return dp[m - 1];
     }
 };
