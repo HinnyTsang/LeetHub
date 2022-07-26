34edf18086569b34797544537c135cdfb8d8e9ad
Hinny Tsang <46905161+HinnyTsang@users.noreply.github.com>
2022-07-26 00:33:00 -0400

    Time: 12 ms (13.42%), Space: 11 MB (75.00%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,50 +1,45 @@
 class Solution {
 private:
-    int binary_search(vector<int>& nums, int& target, int l, int r) {
-        if (r < l) 
-            return -1;
-        if (r == l) 
-            return nums[l] == target ? l: -1;
-        
-        int m;
-        
-        while (r >= l) {
-            m = l + (r - l)/2;
-            if (nums[m] == target) {
-                return m;
-            }
-            else if (nums[m] > target) {
-                r = m - 1;
-            }
-            else {
-                l = m + 1;
-            }
-        }
-        
-        return -1;
+    int binary_search(vector<int>& nums, int l, int r, const int &target);
+    int search(vector<int>& nums, int l, int r, const int &target);
+public:
+    int search(vector<int>& nums, int target) {
+        return search(nums, 0, nums.size() - 1, target);
     }
-    int search(vector<int>& nums, int& target, int l, int r) {
-        
-        if (r < l) 
-            return -1;
-        if (r == l) 
-            return nums[l] == target ? l: -1;
-        
-        if (nums[r] > nums[l])
-            return binary_search(nums, target, l, r);
-        
-        int m = l + (r - l) / 2;
+};
+
+
+int Solution::search(vector<int>& nums, int l, int r, const int &target) {
+    
+    int m = l + ( r - l )/ 2;
+    
+    if (l == r) {
+        return nums[l] == target? l: -1;
+    }
+    
+    if (nums[l] < nums[r])
+        return binary_search(nums, l, r, target);
+    
+    int a = search(nums, l, m, target);
+    
+    if (a != -1) return a;
+    
+    return search(nums, m+1, r, target);
+}
+
+int Solution::binary_search(vector<int>& nums, int l, int r, const int &target) {
+    
+    int m;
+    
+    while (l <= r) {
+        m = l + (r - l)/2;
         
         if (nums[m] == target)
             return m;
-        
-        return 
-            search(nums, target, l, m - 1) != -1?
-            search(nums, target, l, m - 1):
-            search(nums, target, m + 1, r);
+        else if (nums[m] > target) 
+            r = m - 1;
+        else
+            l = m + 1;        
     }
-public:
-    int search(vector<int>& nums, int target) {
-        return search(nums, target, 0, nums.size() - 1);
-    }
-};
+    return -1;
+}
