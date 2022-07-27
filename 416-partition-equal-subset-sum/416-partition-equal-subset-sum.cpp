e9b201fa897cb401b3a7946029a4a7aa127dc348
Hinny Tsang <46905161+HinnyTsang@users.noreply.github.com>
2022-07-26 23:27:09 -0400

    Time: 363 ms (40.62%), Space: 9.1 MB (94.53%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -9,17 +9,15 @@ public:
         
         sum /= 2;
         
-        vector<vector<bool>> dp(n + 1, vector<bool>(sum + 1, false));
-        dp[0][0] = true;
+        vector<bool> dp(sum + 1, false);
+        dp[0] = true;
         
         for (int i = 1; i <= n; ++i) {
-            for (int j = sum; j >= 0; --j) {
-                dp[i][j] = dp[i - 1][j];
-                if (j >= nums[i-1])                
-                    dp[i][j] = dp[i][j] | dp[i - 1][j - nums[i-1]];
+            for (int j = sum; j >= nums[i-1]; --j) {
+                dp[j] = dp[j] | dp[j - nums[i-1]];                
             }
         }
         
-        return dp[n][sum];
+        return dp.back();
     }
 };
